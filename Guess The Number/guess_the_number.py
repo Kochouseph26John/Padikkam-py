8b758b197335e643fd6fc63e8331747a2a12a7f9
Kochouseph John <73066779+Kochouseph26John@users.noreply.github.com>
2023-05-28 13:32:21 +0530

    Update guess_the_number.py

--- Guess The Number/guess_the_number.py
+++ Guess The Number/guess_the_number.py
@@ -1,16 +1,26 @@
 import random
 
-n=random.randint(1,100)
-for i in range(5):
-    f=0
-    g=int(input("Your guess?"))
-    if g==n:
-        print("Congrats!!!...Your guess is right!")
-        f=1
+while True:
+    
+    n=random.randint(1,100)
+    i=0
+    while True:
+        i+=1
+        g=int(input("Your guess?"))
+        if g==n:
+            print("Congrats!!!...Your guess is right!")
+            f=1
+            break
+        elif g>n:
+            print("Your guess is high!")
+        else:
+            print("Your guess is low!")
+        if i == 5:
+            f=0
+            break
+    if f==0:
+        print("You are out of chance")
+        
+    r = input("Do u want to continue (y/n)")
+    if r=='n':
         break
-    elif g>n:
-        print("Your guess is high!")
-    else:
-        print("Your guess is low!")
-if f==0:
-    print("You are out of chance")
